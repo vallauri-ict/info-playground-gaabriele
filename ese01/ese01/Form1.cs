1775230e312609dac9866702c268abb91194e955
Gabriele <62541822+gaabriele@users.noreply.github.com>
2020-09-30 08:31:33 +0200

    Add files via upload

---
+++ ese01/ese01/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ese01
+{
+    public partial class btnForm2 : Form
+    {
+        public btnForm2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void btnForm2B_Click(object sender, EventArgs e)
+        {
+            Form f2b = new Form();
+            f2b.Text ="Aperta Form Dinamica";
+            f2b.Show();
+        }
+    }
+}
