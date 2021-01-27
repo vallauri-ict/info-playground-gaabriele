a2eae2aa4e37f218d11ec03c189f707eff374aa6
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-01-27 08:19:53 +0100

    Add files via upload
    Esercizi 12,13,14,15,17,18,19

---
+++ ese18-OOP-PersoneStudenti/README.md
@@ -0,0 +1,2 @@
+# OOP-PersoneStudenti
+## Esercizio su OOP Es 1 preso da dispense OOP2 
---
+++ ese18-OOP-PersoneStudenti/ese18-OOP-PersoneStudenti/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _018_OOP_PersoneStudenti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Studente> lstStudenti = new List<Studente>();
+
+        private void btnAggiungi_Click(object sender, EventArgs e)
+        {
+            Studente s = new Studente();
+            s.SetAttributs(txtNome.Text, txtCognome.Text, txtSesso.Text, Convert.ToInt32(txtEta.Text));
+            lstStudenti.Add(s);
+            listStudenti.Items.Add(s.GetCognome());
+        }
+
+        private void btnInserisciVoto_Click(object sender, EventArgs e)
+        {
+            string cognome = listStudenti.SelectedItems[0].Text;
+            foreach (Studente item in lstStudenti)
+            {
+                if (item.GetCognome() == listStudenti.SelectedItems[0].Text)
+                {
+                    item.voti.Add(Convert.ToInt32(txtVoto.Text));
+                }
+            }
+        }
+
+        private void btnVissualizza_Click(object sender, EventArgs e)
+        {
+            Studente s = new Studente();
+            s = lstStudenti.Find(stud => stud.GetCognome() == listStudenti.SelectedItems[0].Text);
+            MessageBox.Show("lo studente " + s.GetCognome() + " " + s.GetNome() + " di " + s.Geteta() + " anni, ha la media del " + s.media());
+        }
+    }
+}
